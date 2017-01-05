b79b5c6a9bd38e193fd959d87e3fa8442d529e53
Vanessa <van.gg@live.com.mx>
2017-01-05 01:20:30 -0600

    Add  real images and questions

--- app/controllers/s_pages_controller.rb
+++ app/controllers/s_pages_controller.rb
@@ -22,19 +22,68 @@ class SPagesController < ApplicationController
 	private
 		def do_questions
 			@questions = []
-			3.times do |n| 
+			14.times do |n| 
 				@questions[n] = {}
 				@questions[n][:id] = n
 			end
-			@questions[0][:question] = "¿Cual es1?"
-			@questions[0][:image] = "image0.jpg"
-			@questions[0][:answer] = true
-			@questions[1][:question] = "¿Cual es2?"
-			@questions[1][:image] = "image0.jpg"
-			@questions[1][:answer] = false
-			@questions[2][:question] = "¿Cual es3?"
-			@questions[2][:image] = "image0.jpg"
+			@questions[0][:question] = "En una taquicardia ventrícular ¿Existe onda P?"
+			@questions[0][:image] = ""
+			@questions[0][:answer] = false
+
+			@questions[1][:question] = "¿Se le llama ciclo cardíaco a todos los fenómenos ocurridos en el corazón, desde que inicia un latido hasta el inicio del siguiente latido?"
+			@questions[1][:image] = "p100.jpg"
+			@questions[1][:answer] = true
+
+			@questions[2][:question] = "¿El ciclo cardíaco inicia con el potencial de acción del Nodo S.A?"
+			@questions[2][:image] = "p1.jpg" 
 			@questions[2][:answer] = true
+
+			@questions[3][:question] = "¿El electrocardiograma es un fenómeno volumétrico?"
+			@questions[3][:image] = "p2.jpg"
+			@questions[3][:answer] = false
+			
+			@questions[4][:question] = "¿La onda señalada en la siguiente imagen corresponde a la despolarización ventricular?"
+			@questions[4][:image] = "p8.jpg"
+			@questions[4][:answer] = false
+
+            @questions[5][:question] = "¿El intervalo QT (coloreado de verde en la imagen) depende de la FC?"
+			@questions[5][:image] = "p5.jpg"
+			@questions[5][:answer] = true
+
+            @questions[6][:question] = "¿El segmento (coloreado de rosa en la imagen) mide 0.6 mV?"
+			@questions[6][:image] = "p6.jpg"
+			@questions[6][:answer] = false
+
+            @questions[7][:question] = "¿El ruido cardíaco señalado en la imagen se escucha en atletas de alto rendimiento?"
+			@questions[7][:image] = "p7.jpg"
+			@questions[7][:answer] = true
+
+            @questions[8][:question] = "¿En la siguiente imagen la flecha nos indica una mala comunicación entre la aurículas y los ventrículos?"
+			@questions[8][:image] = "p9.jpg"
+			@questions[8][:answer] = true
+
+            @questions[9][:question] = "¿La siguiente imagen representa una fibrilación ventrícular?"
+			@questions[9][:image] = "p1000.jpg"
+			@questions[9][:answer] = false
+
+            @questions[10][:question] = "¿La precarga es directamente proporcional al retorno vernoso?"
+			@questions[10][:image] = ""
+			@questions[10][:answer] = true
+
+            @questions[11][:question] = "¿La contracción aurícular se encuentra en la fase I del ciclo cardíaco?"
+			@questions[11][:image] = ""
+			@questions[11][:answer] = true
+
+            @questions[12][:question] = "¿La flecha señala el volumen telediastolico?"
+			@questions[12][:image] = "p140.jpg"
+			@questions[12][:answer] = false
+
+            @questions[13][:question] = "¿La onda señalada en la siguiente imagen corresponde a la despolarización aurícular?"
+			@questions[13][:image] = "p8.jpg"
+			@questions[13][:answer] = true
+
+            
+
 		end
 
 		def question_rand(questions)
