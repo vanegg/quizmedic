d890daeb0114579c1116710003d34a751ac38b95
Vanessa <van.gg@live.com.mx>
2017-01-11 17:58:34 -0600

    fix columns css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
 config.serve_static_assets = true
 config.assets.compile = true
-config.assets.js_compressor = :uglifier
 config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
